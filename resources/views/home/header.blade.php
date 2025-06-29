68c582224528ba4baf04685606c9b2e4e458074f
sonpham1928 <pson4282@gmail.com>
2025-06-29 21:42:17 +0700

    fix-header-not-scroll and logo

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,10 +1,9 @@
 <!-- header inner -->
-         <div class="header">
-            <div class="container">
+         <div class="header fixed-top">
                <div class="row">
-                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 ">
                            <a class="navbar-brand" href="/">
-                              <img width="280" style="margin-top: -40px;" src="/images/MOIMOI.png" alt="Logo" >
+                              <img style="margin-left: 30px; margin-top: -8px; width: 250px; height:60px" src="/images/MOIMOI.png" alt="Logo" >
                            </a>
                   </div>
                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 ">
@@ -62,5 +61,4 @@
                      </nav>
                   </div>
                </div>
-            </div>
          </div>
